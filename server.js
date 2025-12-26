177e5dcba5bebe9eb0400e558197fc7d95ec923e
xttort <96690961+xttort@users.noreply.github.com>
2025-12-26 19:33:42 +0300

    add index.html to server js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Раздача статических файлов из папки 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 // Маршруты API
 app.get('/api/hello', (req, res) => {
